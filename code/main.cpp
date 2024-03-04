e2e6f9e3a6a0fe3c77a17c28d0ccf8057aba43f9
sansiff <ljg@henu.edu.cn>
2024-03-04 08:27:48 +0000

    update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,6 @@
-#include <unistd.h>
 #include "server/webserver.h"
 
 int main() {
-    /* 守护进程 后台运行 */
-    //daemon(1, 0); 
-
     WebServer server(
         1316, 3, 60000, false,          // 端口 ET模式 timeoutMs 优雅退出 
         3306, "root", "root", "pdb",    // Mysql配置
